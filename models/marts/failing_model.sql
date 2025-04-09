94223dcd3fd1d54d26e9b88a2ddf10bf2b94b8d5
Elementary <elementary@elementary.com>
2025-04-09 09:19:41 +0000

    Fix division by zero error in failing_model

--- models/marts/failing_model.sql
+++ models/marts/failing_model.sql
@@ -1,2 +1,7 @@
-select 1 / 0
-from {{ ref('all_dates') }}
+select 
+    case 
+        when denominator = 0 then null 
+        else 1 / denominator 
+    end as safe_division
+from {{ ref('all_dates') }} as ad
+cross join (select 0 as denominator) as d
