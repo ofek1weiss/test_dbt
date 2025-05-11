21cc2d8908198554d3a8ad4cac4e7ab3934bcab3
Ofek Weiss <ofek@elementary-data.com>
2025-05-11 13:03:33 +0000

    Fix failing_model: Replace division by zero with valid date query

--- models/marts/failing_model.sql
+++ models/marts/failing_model.sql
@@ -1,2 +1,6 @@
-select 1 / 0
+select
+    date_day,
+    extract(year from date_day) as year,
+    extract(month from date_day) as month,
+    extract(day from date_day) as day
 from {{ ref('all_dates') }}
