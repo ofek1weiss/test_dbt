1c6a6ebab8309218dc70975c5afdbc5d019906a4
Ofek Weiss <ofek@elementary-data.com>
2025-07-08 13:57:03 +0000

    Add comment about potential reference change

--- models/marts/failing_model.sql
+++ models/marts/failing_model.sql
@@ -3,4 +3,4 @@ select
     WHEN 0 != 0 THEN 1 / 0
     ELSE NULL  -- Return NULL instead of error
   END
-from {{ ref('all_dates') }}
+from {{ ref('all_dates') }} -- TODO: This reference should be reviewed and potentially changed
